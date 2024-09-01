ac3994b79b0fcdd92c981837a4d2f607be3f90b4
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-09-01 18:10:19 +0530

    test 03

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = "-e ."
-
-def get_requirements(file_path: str) -> List[str]:
-    requirements = []
-
-    with open(file_path) as f:
-        requirements = f.readlines()
-        requirements = [req.strip() for req in requirements if req.strip() and req.strip() != HYPEN_E_DOT]
-
-    return requirements
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
@@ -36,5 +26,4 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirements("requirements_dev.txt"),
 )
